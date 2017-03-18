254d34dd0270346292f24997534277e33f1ce900
vietplays <vietplaysvn@gmail.com>
2017-03-18 15:48:09 +0700

    initial commit

---
+++ hello-world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by NGUYEN VAN NGHIA on 3/18/17.
+//  Copyright © 2017 vietplays. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
